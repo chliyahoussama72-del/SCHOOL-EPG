10c9815916194989b72ab9a7098b3041727c14ce
chliyahoussama72-del <chliyahoussama72@gmail.com>
2026-03-06 11:58:45 +0000

    Delete LANCER_PROJET.bat

--- LANCER_PROJET.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Lancement du projet School EPG...
-
-start cmd /k "cd backend && npm run dev"
-start cmd /k "cd frontend && npm run dev"
-
-echo Serveurs lances ! Ouvrez votre navigateur.
-pause
